0d456d5b18620470800833ae464f5a450c965bbd
LeitMoth <col.phillips2004@gmail.com>
2025-03-12 22:16:18 -0500

    big wip, shows signs of life, but no sound

--- src/pci/mod.rs
+++ src/pci/mod.rs
@@ -23,13 +23,14 @@ pub fn init_pci() {
     a.play();
 }
 
-lazy_static! {
-    static ref SAMPLE: [i16; 128] = {
-        let mut x = [0; 128];
-        for i in 0..(x.len() / 2) {
-            x[i] = i16::MAX;
-        }
+const WAVSIZE: usize = 0x1000;
 
+lazy_static! {
+    static ref SAMPLE: [i16; WAVSIZE * 2] = {
+        let mut x = [0i16; WAVSIZE * 2];
+        for i in 0..x.len() {
+            x[i] = ((i * 100) % (i16::MAX as usize)) as i16;
+        }
         x
     };
 }
@@ -48,15 +49,15 @@ struct BufferDescriptor {
 
 impl BufferDescriptor {
     fn square() -> Self {
-        let raw_addr: *const [i16; 128] = &raw const *SAMPLE;
+        let raw_addr: *const [i16; WAVSIZE * 2] = &raw const *SAMPLE;
 
         debug_assert!(raw_addr as u64 <= u32::MAX as u64);
         let addr_truncated = raw_addr as u32;
 
         BufferDescriptor {
             physical_addr: addr_truncated,
-            num_samples: 128,
-            control: 0,
+            num_samples: WAVSIZE as u16,
+            control: 1 << 15,
         }
     }
 }
@@ -79,14 +80,27 @@ struct AudioAc97 {
 impl AudioAc97 {
     fn play(&self) {
         pci_config_modify(self.bus, self.slot, 0, 0x1, |x| x | 0b101);
+        // pci_config_modify(self.bus, self.slot, 0, 0xF, |x| {
+        //     x | 0b00000000_00000000_00000011_00000011
+        // });
 
-        // io_space_bar_write::<u16>(self.bar0 + 0x00, 0x0);
-        // io_space_bar_write::<u16>(self.bar0 + 0x02, 0x0);
-        // io_space_bar_write::<u16>(self.bar0 + 0x04, 0x0);
-        // io_space_bar_write::<u16>(self.bar0 + 0x18, 0x0);
-        // io_space_bar_write::<u16>(self.bar1 + 0x2C, 0b11);
+        io_space_bar_write::<u16>(self.bar0 + 0x00, 0x0);
+
+        // set volumes
+        io_space_bar_write::<u16>(self.bar0 + 0x02, 0x0);
+        io_space_bar_write::<u16>(self.bar0 + 0x04, 0x0);
+
+        io_space_bar_write::<u16>(self.bar0 + 0x18, 0x0);
+
+        let samp_front = io_space_bar_read::<u16>(self.bar0 + 0x2C);
+        let samp_surr = io_space_bar_read::<u16>(self.bar0 + 0x2E);
+        let samp_lfe = io_space_bar_read::<u16>(self.bar0 + 0x30);
+        let samp_lr = io_space_bar_read::<u16>(self.bar0 + 0x32);
+        println!("samp {samp_front} {samp_surr} {samp_lfe} {samp_lr}");
+
+        io_space_bar_write::<u16>(self.bar1 + 0x2C, 0b0111);
         let mut v = 10;
-        for i in 0..100000 {
+        for i in 0..1000000 {
             v -= 1;
             print!("");
         }
@@ -120,31 +134,45 @@ impl AudioAc97 {
         println!("Writing BDL pos");
         let address = self.bar1 + 0x10 + 0x0;
         let mut l = BUFFER_DESCRIPTOR_LIST.lock();
+        {
+            let j = l[1].physical_addr;
+            for i in 0..100 {
+                let b = (j + i * 2) as *const u16;
+                print!("[{:#X}]", unsafe { *b });
+            }
+        }
         let raw_addr: *mut [BufferDescriptor; 32] = &raw mut *l;
         debug_assert!(raw_addr as u64 <= u32::MAX as u64);
         let addr_truncated = raw_addr as u32;
         println!("BDL^{:#X}", addr_truncated);
-        for i in 0..32 {
+        for i in 0..64 {
             let h = (addr_truncated + i * 2) as *const u16;
-            print!("<{:04X}>", unsafe { *h });
+            // print!("<{:04X}>", unsafe { *h });
         }
         io_space_bar_write(address, addr_truncated);
 
         println!("Writing number of last valid buffer");
         let address_last_valid_idx = self.bar1 + 0x10 + 0x05;
-        io_space_bar_write::<u8>(address_last_valid_idx, 31);
+        io_space_bar_write::<u8>(address_last_valid_idx, 22);
 
         println!("Setting bit for transferring data");
         io_space_bar_write::<u8>(address_reset, 1);
 
+        let mut w = 0;
         loop {
-            let x = io_space_bar_read::<u16>(self.bar1 + 0x16) & 0x1;
-            if x == 1 {
-                println!("F done");
+            io_space_bar_write::<u8>(address_last_valid_idx, 25);
+            let x = io_space_bar_read::<u16>(self.bar1 + 0x16);
+            if x & 2 == 1 || x & 1 == 1 {
+                println!("F done {x:#b}");
                 break;
             } else {
+                w += 1;
+                if w > 20 {
+                    // break;
+                }
                 let y = io_space_bar_read::<u8>(self.bar1 + 0x14);
-                println!("F {x} {y}");
+                let z = io_space_bar_read::<u16>(self.bar1 + 0x18);
+                println!("F {x:#b} {y} ({z}/{})", SAMPLE.len() / 2);
             }
         }
     }
