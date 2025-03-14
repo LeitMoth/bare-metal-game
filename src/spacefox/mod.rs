05bdba5b552155c27e282235a63c274b068267dd
LeitMoth <col.phillips2004@gmail.com>
2025-03-14 09:50:03 -0500

    add road lines

--- src/spacefox/mod.rs
+++ src/spacefox/mod.rs
@@ -87,6 +87,7 @@ pub struct Game<'a> {
     music: MusicLoop<'a>,
     music_started: bool,
     state: GameState,
+    random: u64,
 }
 
 enum GameState {
@@ -117,13 +118,21 @@ impl<'a> Game<'a> {
                 first_draw: true,
                 need_start: false,
             },
+            random: 0xDEADBEEF,
         }
+    }
+
+    fn rand(&mut self) -> u8 {
+        self.random = 599u64.wrapping_mul(self.random).wrapping_add(4153) % 7919;
+
+        return (self.random % 256) as u8;
     }
 
     pub fn tick(&mut self) {
         if self.music_started {
             self.music.wind();
         }
+        let r = self.rand();
         match self.state {
             GameState::Menu {
                 ref mut first_draw,
@@ -172,12 +181,12 @@ impl<'a> Game<'a> {
                 }
             }
             GameState::SpaceFox(ref mut space_fox) => {
-                if space_fox.update() {
+                if space_fox.update(r) {
                     space_fox.draw();
                 } else {
                     self.state = GameState::GameOver {
                         first_draw: true,
-                        timer: 100,
+                        timer: 50,
                     };
                 }
             }
@@ -218,6 +227,21 @@ impl<'a> Game<'a> {
     }
 
     pub fn key(&mut self, k: DecodedKey) {
+        // hopefully we can get better randomness this way
+        match k {
+            DecodedKey::RawKey(key_code) => {
+                self.random = self
+                    .random
+                    .wrapping_mul((key_code as u64).wrapping_sub(7))
+                    .wrapping_add(key_code as u64);
+            }
+            DecodedKey::Unicode(c) => {
+                self.random = self
+                    .random
+                    .wrapping_mul((c as u64).wrapping_sub(72))
+                    .wrapping_add(c as u64);
+            }
+        }
         match self.state {
             GameState::Menu {
                 ref mut need_start, ..
@@ -228,8 +252,20 @@ impl<'a> Game<'a> {
     }
 }
 
+const GRAD_HOR: &[u8] = "#==----==#".as_bytes();
+
 impl SpaceFox {
     pub fn new() -> Self {
+        for x in 0..BUFFER_WIDTH {
+            let c = GRAD_HOR[x / 8];
+            plot(
+                c as char,
+                x,
+                12,
+                ColorCode::new(Color::LightGray, Color::Black),
+            );
+        }
+
         let mut world = [Default::default(); 30];
 
         world[PLAYER] = Model {
@@ -240,7 +276,7 @@ impl SpaceFox {
 
         world[BLOCK] = Model {
             prims: BLOCK_QUADS,
-            pos: v([-3.0, -3.0, 30.0]),
+            pos: v([-3.0, -3.0, 200.0]),
             scale: 2.0,
         };
 
@@ -258,12 +294,14 @@ impl SpaceFox {
         self.b ^= 1
     }
 
-    pub fn update(&mut self) -> bool {
+    pub fn update(&mut self, rand: u8) -> bool {
         {
             let z = self.world[BLOCK].pos.z;
             self.world[BLOCK].pos.z -= (0.01 * z + 3.0).max(1.0);
             if self.world[BLOCK].pos.z < -5.0 {
-                self.world[BLOCK].pos.z = 300.0;
+                self.world[BLOCK].pos.z = 200.0;
+                let interp = ((rand as f32) / 255.0) * 6.0 - 3.0;
+                self.world[BLOCK].pos.x = interp;
             }
         }
 
@@ -394,6 +432,8 @@ impl SpaceFox {
     pub fn draw(&mut self) {
         let d = self.b ^ 1;
         clear_lines(&self.lines[d], self.end[d]);
+        plot_line(10, 24, 40, 10, '/');
+        plot_line(70, 24, 40, 10, '\\');
         self.end[d] = 0;
         draw_lines(&self.lines[self.b], self.end[self.b]);
         self.swap_buffer();
@@ -471,7 +511,43 @@ pub fn plot_line_depth(&[x1, y1, z1, x2, y2, z2, cbit]: &Line) {
                 ColorCode::new(color, Color::Black),
             );
         }
-        // plot('b', x1, y1, color);
+        if x1 == x2 && y1 == y2 {
+            break;
+        }
+        let e2 = 2 * error;
+        if e2 >= dy {
+            if x1 == x2 {
+                break;
+            }
+            error += dy;
+            x1 += sx;
+        }
+        if e2 <= dx {
+            if y1 == y2 {
+                break;
+            }
+            error += dx;
+            y1 += sy;
+        }
+    }
+}
+
+pub fn plot_line(mut x1: i32, mut y1: i32, x2: i32, y2: i32, c: char) {
+    let dx = i32::abs(x2 - x1);
+    let dy = -i32::abs(y2 - y1);
+    let sx = if x1 < x2 { 1 } else { -1 };
+    let sy = if y1 < y2 { 1 } else { -1 };
+    let mut error = dx + dy;
+
+    for _ in 0..24 {
+        if x1 >= 0 && x1 < BUFFER_WIDTH as i32 && y1 >= 0 && y1 < BUFFER_HEIGHT as i32 {
+            plot(
+                c,
+                x1 as usize,
+                y1 as usize,
+                ColorCode::new(Color::DarkGray, Color::Black),
+            );
+        }
         if x1 == x2 && y1 == y2 {
             break;
         }
@@ -507,12 +583,22 @@ pub fn clear_line(&[x1, y1, _, x2, y2, _, _]: &Line) {
 
     for _ in 0..24 {
         if x1 >= 0 && x1 < BUFFER_WIDTH as i32 && y1 >= 0 && y1 < BUFFER_HEIGHT as i32 {
-            plot(
-                ' ',
-                x1 as usize,
-                y1 as usize,
-                ColorCode::new(Color::Black, Color::Black),
-            );
+            if y1 == 12 {
+                let c = GRAD_HOR[x1 as usize / 8];
+                plot(
+                    c as char,
+                    x1 as usize,
+                    y1 as usize,
+                    ColorCode::new(Color::LightGray, Color::Black),
+                );
+            } else {
+                plot(
+                    ' ',
+                    x1 as usize,
+                    y1 as usize,
+                    ColorCode::new(Color::Black, Color::Black),
+                );
+            }
         }
 
         let e2 = 2 * error;
