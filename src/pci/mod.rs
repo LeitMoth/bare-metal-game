9b6a5f7ff60c9e0b65a770fc90ddd8cd7a03dd99
LeitMoth <col.phillips2004@gmail.com>
2025-03-14 02:12:38 -0500

    implement MusicLoop and AudioAc97 functions

--- src/pci/mod.rs
+++ src/pci/mod.rs
@@ -16,6 +16,19 @@ mod io;
 static WAV_DATA: &[u8] = include_bytes!("../../../../../../Documents/something_like_megaman2.raw");
 // static WAV_DATA: &[u8] = include_bytes!("../../../../../../Documents/snippet.raw");
 
+// https://larsimmisch.github.io/pyalsaaudio/terminology.html
+// Fixed by AC97 card
+const NUM_BUFFERS: usize = 32;
+const MAX_SAMPLES_PER_BUF: u16 = 0xFFFE;
+// Defaults that we won't change
+const SAMPLE_SIZE: usize = size_of::<i16>();
+const NUM_CHANNELS: usize = 2;
+// good to know
+const SAMPLES_PER_BUF: u16 = MAX_SAMPLES_PER_BUF;
+const BYTES_PER_BUF: u32 = SAMPLES_PER_BUF as u32 * SAMPLE_SIZE as u32;
+// const SAMPLES_PER_FRAME: usize = NUM_CHANNELS;
+// const FRAMES_PER_BUF: usize = SAMPLES_PER_BUF as usize / SAMPLES_PER_FRAME;
+
 #[repr(packed)]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 struct BufferDescriptor {
@@ -28,14 +41,160 @@ struct BufferDescriptor {
     control: u16,
 }
 
+struct MusicLoop<'a> {
+    ac97: AudioAc97,
+    music_data: &'a [i16],
+    music_data_read_head: usize,
+    sound_blob: &'a mut [i16],
+    last_buffer_filled: u8,
+}
+
+impl<'a> MusicLoop<'a> {
+    fn wind(&mut self) {
+        debug_assert!(NUM_BUFFERS == 32); // if this changes, the bit mask won't work;
+        const MOD32_MASK: u8 = 0b11111;
+        let buf: u8 = self.ac97.get_current_buffer();
+
+        let fill_to = buf.wrapping_sub(1) & MOD32_MASK;
+
+        let mut i = (self.last_buffer_filled + 1) & MOD32_MASK;
+        while i != fill_to {
+            let mut buf_write_head = 0;
+            while buf_write_head < SAMPLES_PER_BUF {
+                let write_pos = i as usize * BYTES_PER_BUF as usize + buf_write_head as usize;
+                self.sound_blob[write_pos] = self.music_data[self.music_data_read_head];
+                buf_write_head += 1;
+                self.music_data_read_head += 1;
+                if self.music_data_read_head >= self.music_data.len() {
+                    self.music_data_read_head = 0;
+                }
+            }
+
+            i = (i + 1) & MOD32_MASK;
+        }
+
+        self.ac97.set_filled_up_to(fill_to);
+    }
+}
+
 #[derive(Debug)]
 struct AudioAc97 {
     bus: u8,
     slot: u8,
+
+    // Native Audio Mixer registers
     // reset, device selection, volume control
-    bar0: u16,
-    // audio data
-    bar1: u16,
+    mixer_port_base: u16,
+
+    // Native Audio Bus Master registers
+    // manages the ring buffer
+    buffer_port_base: u16,
+}
+
+impl AudioAc97 {
+    // buffer_port_base / nabm offsets
+    const GLOBAL_CONTROL: u16 = 0x2C;
+    const PCM_OUT: u16 = 0x10;
+    const LAST_VALID_ENTRY_OFFSET: u16 = 0x05;
+    const CURRENT_PROCESSED_ENTRY_OFFSET: u16 = 0x04;
+    const TRANSFER_CONTROL_OFFSET: u16 = 0x0B;
+
+    fn set_filled_up_to(&self, buf: u8) {
+        debug_assert!((buf as usize) < NUM_BUFFERS);
+
+        let last_valid_entry =
+            self.buffer_port_base + Self::PCM_OUT + Self::LAST_VALID_ENTRY_OFFSET;
+        io_space_bar_write::<u8>(last_valid_entry, buf);
+    }
+
+    fn get_current_buffer(&self) -> u8 {
+        let buf = io_space_bar_read::<u8>(
+            self.buffer_port_base + Self::PCM_OUT + Self::CURRENT_PROCESSED_ENTRY_OFFSET,
+        );
+        debug_assert!((buf as usize) < NUM_BUFFERS);
+        buf
+    }
+
+    // I reffered heavily to https://wiki.osdev.org/AC97
+    // and peeked a few times at the refernced BleskOS driver.
+    // I'm not sure how to properly cite BleskOS, as I didn't directly copy code
+    // (it is in C, so I quite literally could not have direcly copied anyhing),
+    // but I did copy some of the order and wait timings of initialization
+    // and cleared up some, in my opinion, misleading things on the osdev wiki
+    fn init(&self) {
+        // Blesk inserts several 'wait's in its code.
+        // This makes me worry that if I don't do the same,
+        // things may randomly fail if I happen to write too fast
+        // while the card is resetting something.
+        // So I attempt to wait in a few places.
+        fn wait() {
+            //TODO(colin), maybe set this to tick boundaries?
+        }
+
+        // https://wiki.osdev.org/AC97#Detecting_AC97_sound_card
+        // the wiki says we have to write these bits to the AC97 pci control register before
+        // anything else
+        pci_config_modify(self.bus, self.slot, 0, 0x1, |x| x | 0b101);
+
+        // Blesk does this in a different spot, osdev doesn't say to do it at all
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x00, 0xFFFF);
+
+        // This is not from the wiki, but Blesk does this first
+        const RESUME_OPERATION: u32 = 1 << 1;
+        io_space_bar_write::<u32>(
+            self.buffer_port_base + Self::GLOBAL_CONTROL,
+            RESUME_OPERATION,
+        );
+
+        // Blesk waits after it writes to global control
+        wait();
+
+        // osdev.org says to set our volumes now, 0x0 is full volume
+        // TODO(colin): clean up magic constants here
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x18, 0x0); //PCM
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x02, 0x0); //Master
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x04, 0x0); //Aux output
+
+        // osdev.org says: "Set reset bit of output channel
+        // (NABM register 0x1B, value 0x2) and wait for card to clear it""
+        let pcm_out_transfer =
+            self.buffer_port_base + Self::PCM_OUT + Self::TRANSFER_CONTROL_OFFSET;
+        io_space_bar_write::<u8>(pcm_out_transfer, 0b10);
+        while io_space_bar_read::<u8>(pcm_out_transfer) & 0b10 != 0 {
+            wait();
+        }
+
+        // to start playing a sound osdev.org says we still have to do:
+        // - Write physical position of BDL to Buffer Descriptor Base Address register (NABM register 0x10)
+        // - Write number of last valid buffer entry to Last Valid Entry register (NABM register 0x15)
+        // - Set bit for transfering data (NABM register 0x1B, value 0x1)
+        // but here I move that to the begin_transfer function
+    }
+
+    // init() must be called first!
+    fn begin_transfer(&self, bdl_phys_addr: u32, initial_valid_bufs: u8) {
+        debug_assert!((initial_valid_bufs as usize) < NUM_BUFFERS);
+        // to start playing a sound osdev.org says we still have to:
+        // - Write physical position of BDL to Buffer Descriptor Base Address register (NABM register 0x10)
+        // - Write number of last valid buffer entry to Last Valid Entry register (NABM register 0x15)
+        // - Set bit for transfering data (NABM register 0x1B, value 0x1)
+
+        io_space_bar_write(self.buffer_port_base + 0x10, bdl_phys_addr);
+
+        let last_valid_entry =
+            self.buffer_port_base + Self::PCM_OUT + Self::LAST_VALID_ENTRY_OFFSET;
+        io_space_bar_write::<u8>(last_valid_entry, initial_valid_bufs);
+
+        // This is the line that gives Qemu a "volume meter" in pavucontrol
+        // before this, there is no volume indicator, but after this there is!
+        // If the BDL or the data that any entry in it points to is
+        // set up incorrectly, the volume indicator for Qemu should show up,
+        // but not show any activity.
+        const TRANSFER_SOUND_DATA: u8 = 1 << 0;
+        let pcm_out_transfer =
+            self.buffer_port_base + Self::PCM_OUT + Self::TRANSFER_CONTROL_OFFSET;
+        io_space_bar_write::<u8>(pcm_out_transfer, TRANSFER_SOUND_DATA);
+    }
 }
 
 /*
@@ -64,20 +223,6 @@ pub fn init_pci(phys_alloc: &mut PhysAllocator) {
 
     type BDL = [Volatile<BufferDescriptor>; 32];
     let bdl = phys_alloc.alloc32::<BDL>();
-
-    // https://larsimmisch.github.io/pyalsaaudio/terminology.html
-    // Fixed by AC97 card
-    const NUM_BUFFERS: usize = 32;
-    const MAX_SAMPLES_PER_BUF: u16 = 0xFFFE;
-    // Defaults that we won't change
-    const SAMPLE_SIZE: usize = size_of::<i16>();
-    const NUM_CHANNELS: usize = 2;
-
-    // calculuation
-    const SAMPLES_PER_BUF: u16 = MAX_SAMPLES_PER_BUF;
-    const BYTES_PER_BUF: u32 = SAMPLES_PER_BUF as u32 * SAMPLE_SIZE as u32;
-    // const SAMPLES_PER_FRAME: usize = NUM_CHANNELS;
-    // const FRAMES_PER_BUF: usize = SAMPLES_PER_BUF as usize / SAMPLES_PER_FRAME;
 
     const SAMPLES_IN_BLOB: usize = SAMPLES_PER_BUF as usize * NUM_BUFFERS;
     type SamplesBlob = [Volatile<i16>; SAMPLES_IN_BLOB];
@@ -114,37 +259,37 @@ impl AudioAc97 {
         //     x | 0b00000000_00000000_00000011_00000011
         // });
 
-        io_space_bar_write::<u32>(self.bar1 + 0x2C, 0x2);
+        io_space_bar_write::<u32>(self.buffer_port_base + 0x2C, 0x2);
 
         for i in 0..100_000 {
             print!("");
         }
 
-        io_space_bar_write::<u16>(self.bar0 + 0x00, 0xFF);
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x00, 0xFF);
 
-        let samp_front = io_space_bar_read::<u16>(self.bar0 + 0x2C);
-        let samp_surr = io_space_bar_read::<u16>(self.bar0 + 0x2E);
-        let samp_lfe = io_space_bar_read::<u16>(self.bar0 + 0x30);
-        let samp_lr = io_space_bar_read::<u16>(self.bar0 + 0x32);
+        let samp_front = io_space_bar_read::<u16>(self.mixer_port_base + 0x2C);
+        let samp_surr = io_space_bar_read::<u16>(self.mixer_port_base + 0x2E);
+        let samp_lfe = io_space_bar_read::<u16>(self.mixer_port_base + 0x30);
+        let samp_lr = io_space_bar_read::<u16>(self.mixer_port_base + 0x32);
         println!("samp {samp_front} {samp_surr} {samp_lfe} {samp_lr}");
 
         {
             let (pcm, master, aux) = (
-                io_space_bar_read::<u16>(self.bar0 + 0x18), //PCM
-                io_space_bar_read::<u16>(self.bar0 + 0x02), //Master
-                io_space_bar_read::<u16>(self.bar0 + 0x04), //Aux output
+                io_space_bar_read::<u16>(self.mixer_port_base + 0x18), //PCM
+                io_space_bar_read::<u16>(self.mixer_port_base + 0x02), //Master
+                io_space_bar_read::<u16>(self.mixer_port_base + 0x04), //Aux output
             );
 
             println!("PCM,MASTER,AUX {pcm:#X} {master:#X} {aux:#X} {pcm:#b} {master:#b} {aux:#b}")
         }
         {
-            let glob = io_space_bar_read::<u32>(self.bar1 + 0x2C);
+            let glob = io_space_bar_read::<u32>(self.buffer_port_base + 0x2C);
             // note that this is just 0. Does that mean device is is reset?
             println!("global control {glob:#b}");
         }
 
         println!("Setting reset bit of audio...");
-        let address_reset = self.bar1 + 0x1B;
+        let address_reset = self.buffer_port_base + 0x1B;
         io_space_bar_write::<u8>(address_reset, 2);
         // pci_config_modify(self.bus, self.slot, 0, 0x1, |x| x & !0b11);
         // let b = io_space_bar_read::<u8>(address_reset);
@@ -164,23 +309,23 @@ impl AudioAc97 {
         // pci_config_modify(self.bus, self.slot, 0, 0x1, |x| x | 0b101);
 
         // set volumes
-        io_space_bar_write::<u16>(self.bar0 + 0x18, 0x0); //PCM
-        io_space_bar_write::<u16>(self.bar0 + 0x02, 0x0); //Master
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x18, 0x0); //PCM
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x02, 0x0); //Master
 
-        io_space_bar_write::<u16>(self.bar0 + 0x04, 0x2020); //Aux output
+        io_space_bar_write::<u16>(self.mixer_port_base + 0x04, 0x2020); //Aux output
         for i in 0..100_000 {
             print!("");
         }
 
         io_space_bar_write::<u8>(address_reset, 0x0);
 
-        io_space_bar_write(self.bar1 + 0x10, bdl_phys_loc);
+        io_space_bar_write(self.buffer_port_base + 0x10, bdl_phys_loc);
 
         println!("Writing number of last valid buffer");
-        let address_last_valid_idx = self.bar1 + 0x10 + 0x05;
+        let address_last_valid_idx = self.buffer_port_base + 0x10 + 0x05;
         io_space_bar_write::<u8>(address_last_valid_idx, 10);
 
-        io_space_bar_write::<u16>(self.bar1 + 0x16, 0x1C);
+        // io_space_bar_write::<u16>(self.buffer_port_base + 0x16, 0x1C);
 
         // IMPORTANT:
         // This is the line that gives Qemu a "volume meter" in pavucontrol
@@ -190,9 +335,9 @@ impl AudioAc97 {
 
         let mut w = 0;
         loop {
-            let y = io_space_bar_read::<u8>(self.bar1 + 0x14);
+            let y = io_space_bar_read::<u8>(self.buffer_port_base + 0x14);
             io_space_bar_write::<u8>(address_last_valid_idx, y.wrapping_sub(1) & 0b11111);
-            let x = io_space_bar_read::<u16>(self.bar1 + 0x16);
+            let x = io_space_bar_read::<u16>(self.buffer_port_base + 0x16);
 
             if x & 2 == 1 || x & 1 == 1 {
                 println!("F done {x:#b}");
@@ -202,11 +347,11 @@ impl AudioAc97 {
                 if w > 1 {
                     // break;
                 }
-                let y = io_space_bar_read::<u8>(self.bar1 + 0x14);
-                let z = io_space_bar_read::<u16>(self.bar1 + 0x18);
+                let y = io_space_bar_read::<u8>(self.buffer_port_base + 0x14);
+                let z = io_space_bar_read::<u16>(self.buffer_port_base + 0x18);
 
-                let next = io_space_bar_read::<u8>(self.bar1 + 0x1A);
-                let control = io_space_bar_read::<u8>(self.bar1 + 0x1B);
+                let next = io_space_bar_read::<u8>(self.buffer_port_base + 0x1A);
+                let control = io_space_bar_read::<u8>(self.buffer_port_base + 0x1B);
 
                 println!("F {x:#b} {y} ({z:#06X}/) {next} {control:#010b}");
             }
@@ -264,8 +409,8 @@ fn init_audio() -> Option<AudioAc97> {
                     audio = Some(AudioAc97 {
                         bus,
                         slot: device,
-                        bar0: (full_header.base_addresses[0] & 0xFFFFFFFC) as u16,
-                        bar1: (full_header.base_addresses[1] & 0xFFFFFFFC) as u16,
+                        mixer_port_base: (full_header.base_addresses[0] & 0xFFFFFFFC) as u16,
+                        buffer_port_base: (full_header.base_addresses[1] & 0xFFFFFFFC) as u16,
                     });
                 }
             }
